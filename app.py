e70ac73f84d62948121f383891d98fec097297bf
Khanh Nghiem <knghiem@conncoll.edu>
2018-01-17 18:36:19 -0500

    ADI Labs Challenge Submission

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET'])
 def main():
     return render_template('index.html')
 
+@app.route('/kvn2112', methods=['GET'])
+def mypage():
+    return render_template('khanh.html')
 
-if __name__ == '__main__':
-    app.run()
